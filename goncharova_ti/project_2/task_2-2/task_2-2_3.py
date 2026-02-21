2a2a7e0de3098d700dfa21be8b586577d97b2341
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-02-21 11:06:01 +0300

    Create task_2-2_3.py

---
+++ goncharova_ti/project_2/task_2-2/task_2-2_3.py
@@ -0,0 +1,6 @@
+device = 'vacuum cleaner'
+inventory_number = '639000073663733212'
+condition = 'working properly'
+quantity = '11'
+
+print(f'Device\t                {device}\nInventory number\t{inventory_number}\nCondition\t        {condition}\nQuantity\t        {quantity}')

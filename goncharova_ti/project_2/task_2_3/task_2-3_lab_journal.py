49daed5aa94fb3156bf2d010ca086ee9276d48ef
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-02-22 18:14:54 +0300

    Create task_2-3_lab_journal.py

---
+++ goncharova_ti/project_2/task_2_3/task_2-3_lab_journal.py
@@ -0,0 +1,7 @@
+name = input('Введите имя исследователя: ')
+data = input('Дата эксперимента: ')
+name_of_experiment = input('Название эксперимента: ')
+conclusion = input('Результат:')
+
+with open('journal.txt', 'w') as f:
+  

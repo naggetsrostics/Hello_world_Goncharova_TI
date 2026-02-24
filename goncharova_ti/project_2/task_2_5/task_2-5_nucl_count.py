1499db6f551be8bd21c3b38cb2ff747ef364a442
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-02-24 23:15:33 +0300

    Update task_2-5_nucl_count.py

--- goncharova_ti/project_2/task_2_5/task_2-5_nucl_count.py
+++ goncharova_ti/project_2/task_2_5/task_2-5_nucl_count.py
@@ -1,4 +1,7 @@
 print('=== Анализ последовательности ДНК ===')
-nucleo = input('Введите последовательность ДНК: ')
-print('Последовательность в верхнем регистре:', nucleo.upper())
-print(f'Подсчёт нуклеотидов:\nA: {nucleo.count('A')}\nT: {nucleo.count('T')}\nG: {nucleo.count('G')}\nC: {nucleo.count('C')}\n\nОбщая длина: {len(nucleo)} нуклеотидов')
+print('Введите последовательность ДНК: ', end='')
+with open("C:/Users/tanya/OneDrive/Desktop/Гончарова.txt", 'r', encoding='utf-8') as f:
+  nucleo = f.read()
+  print(nucleo)
+  print('Последовательность в верхнем регистре:', nucleo.upper())
+  print(f'Подсчёт нуклеотидов:\nA: {nucleo.count('A')}\nT: {nucleo.count('T')}\nG: {nucleo.count('G')}\nC: {nucleo.count('C')}\n\nОбщая длина: {len(nucleo)} нуклеотидов')

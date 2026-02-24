18b8fdd41ce3a35d89d456ce4dcbde6b063346cc
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-02-24 23:05:18 +0300

    Update task_2-4_lab_assistant.py

--- goncharova_ti/project_2/task_2_4/task_2-4_lab_assistant.py
+++ goncharova_ti/project_2/task_2_4/task_2-4_lab_assistant.py
@@ -1,4 +1,4 @@
 V = float(input("Необходимыйобъем раствора (мл): "))
 M = round(V*0.009, 2)
-with open('recipe.txt', 'w', encoding='utf-8') as f:
+with open('C:/Users/tanya/OneDrive/Desktop/recipe.txt', 'w', encoding='utf-8') as f:
   f.write(f'ОТЧЕТ ПО ПРИГОТОВЛЕНИЮ:\n-----------------------\nОбщий объем:\t{V} мл\nМасса соли:\t{M} г\nОбъем воды:\t{V} мл')

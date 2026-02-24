f59753706b2510bdd16cb6414599ecc60ba843fc
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-02-24 23:04:51 +0300

    Update task_2_3-recipe.py

--- goncharova_ti/project_2/task_2_3/task_2_3-recipe.py
+++ goncharova_ti/project_2/task_2_3/task_2_3-recipe.py
@@ -1,7 +1,7 @@
 nutrient_medium = input("Введите название питательной среды: ")
 agar_concentration = input("Введите концентрацию агара (%): ")
 sterilization_temperature = input("Введите температуру стерилизации ( °C): ")
-with open("recipe.txt", "w", encoding="utf-8") as f:
+with open("C:/Users/tanya/OneDrive/Desktop/recipe.txt", "w", encoding="utf-8") as f:
     f.write(f"{nutrient_medium.upper()}\n")
     f.write(f"Концентрация агара:\t        {agar_concentration}\nТемпература стерилизации:\t{sterilization_temperature}")
 print("Файл 'recipe.txt' успешно сформирован!")

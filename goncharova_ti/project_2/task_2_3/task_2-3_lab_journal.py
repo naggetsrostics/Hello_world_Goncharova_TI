4f7848f1d53682840364561308cf21014ce587f2
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-02-24 22:59:05 +0300

    Update task_2-3_lab_journal.py

--- goncharova_ti/project_2/task_2_3/task_2-3_lab_journal.py
+++ goncharova_ti/project_2/task_2_3/task_2-3_lab_journal.py
@@ -5,19 +5,18 @@ conclusion = input('Результат:')
 c1 = '| ФИО исследователя: ' + name
 c2 = '| Дата             : ' + data
 c3 = '| Эксперимент      : ' + name_of_experiment
-with open('journal.txt', 'w') as f:
-  print('+--------------------------------------------------+\n| Электронный лабораторный журнал                  |\n+--------------------------------------------------+', file=f)
-  print(c1+ ' '*(51-len(c1))+ '|', sep='', file=f)
-  print(c2+ ' '*(51-len(c2))+ '|', sep='', file=f)                    
-  print(c3+ ' '*(51-len(c3))+ '|', sep='', file=f)
-  print('+--------------------------------------------------+', file=f)
-  print('| Вывод:' + 43*' ', '|', sep='', file=f)
+with open("C:/Users/tanya/OneDrive/Desktop/journal.txt", 'w', encoding='utf-8') as f:
+  print('+----------------------------------------------------------------------+\n| Электронный лабораторный журнал                                      |\n+----------------------------------------------------------------------+', file=f)
+  print(c1+ ' '*(71-len(c1))+ '|', sep='', file=f)
+  print(c2+ ' '*(71-len(c2))+ '|', sep='', file=f)                    
+  print(c3+ ' '*(71-len(c3))+ '|', sep='', file=f)
+  print('+----------------------------------------------------------------------+', file=f)
+  print('| Вывод:' + 63*' ', '|', sep='', file=f)
 
   conclusion = conclusion.split()
   while len(conclusion) >= 4:
       line = ' '.join(conclusion[:5])
-      print(f'| {line}' + (49-len(line))*' ' + '|', file=f)
+      print(f'| {line}' + (69-len(line))*' ' + '|', file=f)
       conclusion = conclusion[5:]
-  print(f'| {' '.join(conclusion)}' + (49-len(conclusion[0]))*' ' + '|', file=f)
-  print('+--------------------------------------------------+', file=f)
-  # не получается на общий вывод делать красивую табличку, если слова длинные. сделала по примеру вывода. возможно. просто надо сделать таблицу шире, чтобы слова влезали
+  print(f'| {' '.join(conclusion)}' + (69-len(conclusion))*' ' + '|', file=f)
+  print('+----------------------------------------------------------------------+', file=f)

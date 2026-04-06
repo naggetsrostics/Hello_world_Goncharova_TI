c233c38e98b6abda727b3c6c159b2d6d00a09183
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-04-06 23:38:44 +0300

    Update task_4-13-11.py

--- goncharova_ti/projects_4/task_4_13/task_4-13-11.py
+++ goncharova_ti/projects_4/task_4_13/task_4-13-11.py
@@ -2,9 +2,11 @@ l = list(map(int, input('Введите числа через пробел:').sp
 n = len(l)
 sum = 0
 count = 0
+num = 0
 while count < n:
   if count % 2 == 0 :
     sum += l[count]
+    cum += 1
   count += 1
-sr = sum / n
+sr = sum / num
 print(sr)  

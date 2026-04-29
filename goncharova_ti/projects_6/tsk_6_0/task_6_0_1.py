a9a4b51fbcfb655f0292cc5af2a645047b5fe9d1
Гончарова Татьяна Ивановна <tanyaagoncharovaa@gmail.com>
2026-04-29 16:24:57 +0300

    Delete goncharova_ti/projects_6/tsk_6_0 directory

--- goncharova_ti/projects_6/tsk_6_0/task_6_0_1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("C:/Users/tanya/Downloads/wild_boars.csv")
-print(df['tusk_length_cm'])
-print(df['tusk_length_cm'].max())
-print(df['tusk_length_cm'].min())
--- goncharova_ti/projects_6/tsk_6_0/task_6_0_6.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-with open("C:/Users/tanya/OneDrive/Desktop/task_6/percentiles_lenght.txt", "w", encoding='utf-8') as f:
-    df = pd.read_csv("C:/Users/tanya/Downloads/wild_boars.csv")
-    q1 = df.groupby('gender')['length_cm'].quantile(0.25)
-    q3 = df.groupby('gender')['length_cm'].quantile(0.75)
-    print("IQR:", file=f)
-    print(q3-q1, file=f)
